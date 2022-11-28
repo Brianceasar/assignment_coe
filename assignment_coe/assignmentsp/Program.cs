9df88eac6cd50d7ee76dbfe746554a28e9a31d9a
Brianceasar <95188858+Brianceasar@users.noreply.github.com>
2022-11-28 10:07:46 +0300

    Added a prgrogram that prints first 100 members of a sequence(qn2)

--- assignment_coe/assignmentsp/Program.cs
+++ assignment_coe/assignmentsp/Program.cs
@@ -3,7 +3,42 @@
 //number 1
 
 //Write a program that prints the first 100 members of the sequence 2, -3, 4, -5, 6, -7, 8
+// Solution 1
 
+/*public class Sequence
+{
+    static void Main(string[] args)
+    {
+        for (int i = 2; i < 102; i++)
+        {
+            if (i % 2 == 0)
+
+            Console.WriteLine( "{0}, {1}, ", i, (i + 1) * (-1));
+        }
+    }
+}
+*/
+
+// Solution 2
+public class Sequence
+{
+    static void Main (string[] args)
+    {
+        for (int i = 2; i < 101; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.Write("{0} ", i);
+            }
+            else
+            {
+                Console.Write("{0} ", -i);
+            }
+        }
+
+        Console.WriteLine();
+    }
+}
 //number 2
 
 // Write a program that reads your age from the console and prints your age after 10 years.
@@ -35,7 +70,7 @@
 
 // Write a program that reads an integer number n from the console and prints all numbers in the range [1…n], each on a separate line.
 
-public class nInterger
+/*public class nInterger
 {
     public static void Main()
     {
@@ -49,7 +84,7 @@ public class nInterger
  
         Console.WriteLine(String.Join(", ", array));
     }
-}
+}*/
 
 // number 4
 
