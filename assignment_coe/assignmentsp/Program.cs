14a86d707147c74ec0a3d6a91576f45de93416ee
emmanuelkussy <emmanuelkusiluka@gmail.com>
2022-11-28 06:54:09 +0300

    fibonacc

--- assignment_coe/assignmentsp/Program.cs
+++ assignment_coe/assignmentsp/Program.cs
@@ -50,3 +50,9 @@ public class nInterger
         Console.WriteLine(String.Join(", ", array));
     }
 }
+
+// number 4
+
+//Write a program that prints on the console the first 100 numbers in the Fibonacci sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, …
+
+// what the hell is fibonacci doing humu 
